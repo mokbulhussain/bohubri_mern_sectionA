17a3457b3e10d9a6eb105ca74358322b38a275cf
Mokbul Hussain <mokbulhussain2@gmail.com>
2023-07-04 12:25:27 +0600

    javascript variable

--- js/script.js
+++ js/script.js
@@ -25,9 +25,10 @@ console.log("work");
 
 
 
-//------------  user input ----------
+//-----------------  user input -------------------
 
 
+/*
 let x;
 
 x=prompt("Please Enter Your Name");
@@ -35,7 +36,51 @@ x=prompt("Please Enter Your Name");
 document.write(x);
 console.log(x);
 
+*/
 
 
+
+
+
+//-----------------  variable --------------------------------
+
+// var , let
+
+var a=50;
+console.log(a);
+a=70;
+console.log(a);
+
+
+let b=40;
+
+let c=a+b;
+console.log(c);
+
+
+// JAVASCRIPT case sensetive
+// diffrent variable and diffrent value.
+let myname, MYNAME;
+myname="hasan";
+MYNAME="korim";
+
+console.log(myname);
+console.log(MYNAME);
+
+
+// const variable
+// const variable value not change.
+const d=80;
+console.log(d);
+
+
+
+
+
+// --------------------- keywords -------------
+
+// javascript keywords hosse reserved word. ay gula variable name use kora jabe na.
+
+// exm: var, let const, function , class , console, prompt
 
 
