353c93cc403b9c5c333873e45137dd4bb646896b
Mokbul Hussain <mokbulhussain2@gmail.com>
2023-07-04 13:55:19 +0600

    arithmatic operator

--- js/script.js
+++ js/script.js
@@ -44,6 +44,9 @@ console.log(x);
 
 //-----------------  variable --------------------------------
 
+/*
+
+
 // var , let
 
 var a=50;
@@ -82,5 +85,134 @@ console.log(d);
 // javascript keywords hosse reserved word. ay gula variable name use kora jabe na.
 
 // exm: var, let const, function , class , console, prompt
+
+
+*/  
+
+
+
+
+
+//-------------------- Arithmetic Operators -----------------------
+
+// addition (+)
+// sub (-)
+// multiplcation (*)
+// divide (/)
+// modulas (%)
+// power (**)
+// increment (++)
+// decrement (--)
+
+
+// addition (+)
+/*
+let a=20;
+let b= 30;
+let c=a+b;
+console.log(c)
+*/
+
+
+// sub (-)
+/*
+let a=60;
+let b= 30;
+let c=a-b;
+console.log(c)
+*/
+
+
+// multi (*)
+
+/*
+let a=60;
+let b= 30;
+let c=a*b;
+console.log(c)
+
+*/
+
+// divide (/)
+
+/*
+let a=60;
+let b= 30;
+let c=a/b;
+console.log(c)
+*/
+
+
+// modulas (%)
+// modulas mane hosse bagshesh ja take ta.
+/*
+let a=63;
+let b= 30;
+let c=a%b;
+console.log(c)
+*/
+
+
+
+// modulas (%)
+// modulas mane hosse bagshesh ja take ta.
+/*
+let a=63;
+let b= 30;
+let c=a%b;
+console.log(c)
+*/
+
+
+// power (**)
+
+/*
+console.log(2**3)
+
+
+// increment (++)
+let a=6;
+// a++;
+// console.log(a);
+
+let c;
+// first a assign kore tarpor increment kore.
+
+c=a++
+console.log(c);
+// increment korse
+// console.log(a)
+// increment kore asign korse
+c=a;
+console.log(c)
+c=++a;
+console.log(c);
+
+*/
+
+
+
+// increment (++)
+let a=6;
+// a--;
+// console.log(a);
+
+let c;
+// first a assign kore tarpor increment kore.
+
+c=a--
+console.log(c);
+// increment korse
+console.log(a)
+// increment kore asign korse
+c=a;
+console.log(c)
+c=--a;
+console.log(c);
+
+
+
+
+
 
 
